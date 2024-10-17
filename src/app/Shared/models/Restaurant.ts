48483edbed6b875e565c4b5a47dc7e73b7b86dd8
ravishansenevirathna <travishan55@gmail.com>
2024-10-17 20:54:12 +0530

    first commit

---
+++ src/app/Shared/models/Restaurant.ts
@@ -0,0 +1,8 @@
+
+export interface Restaurant {
+  id?: number;
+  name?: string;
+  address?: string ;
+  city?: string;
+  restaurantDescription?: string;
+}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,15 +3,25 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import {HeaderModule} from "./header/header.module";
+import {RestaurantListingModule} from "./restaurant-listing/restaurant-listing.module";
+import {HttpClientModule} from "@angular/common/http";
+import {FoodCatalogueModule} from "./food-catalogue/food-catalogue.module";
+import {OrderSummaryModule} from "./order-summary/order-summary.module";
 
 @NgModule({
   declarations: [
     AppComponent
   ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+    imports: [
+        BrowserModule,
+        AppRoutingModule,
+        HeaderModule,
+        RestaurantListingModule,
+        HttpClientModule,
+      FoodCatalogueModule,
+      OrderSummaryModule
+    ],
   providers: [],
   bootstrap: [AppComponent]
 })
